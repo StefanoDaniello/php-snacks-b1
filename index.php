d59ca21532a43f3593b0299b637c455a3d47fb99
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-30 16:43:43 +0200

    snack5

--- index.php
+++ index.php
@@ -81,7 +81,9 @@
             $numeri_casuali[] = $controllo_numeri;
         }
     }
-   
+    // snack 5
+    $paragrafo='Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora rem quae cum inventore accusantium temporibus corporis est necessitatibus nemo enim nostrum mollitia nesciunt odio, suscipit libero voluptates eligendi officia error quasi vitae, reprehenderit possimus, consectetur non exercitationem! Nam excepturi laboriosam harum cupiditate dolorem ratione totam commodi quis unde. Hic harum architecto reiciendis accusantium a sit id nobis sapiente tempora aliquid cumque eaque vel mollitia, incidunt voluptates! Odio debitis quae culpa rem obcaecati dolores illo totam beatae unde? Sed soluta consequatur voluptas minus ipsam ex aspernatur quas optio beatae velit quasi dolor pariatur, obcaecati, itaque nisi tempore. Est saepe excepturi fuga.';
+    $newParagrafo = explode("." , $paragrafo)
 ?>
 
 <!DOCTYPE html>
@@ -124,7 +126,12 @@
      foreach($numeri_casuali as $numero){
         echo $numero . "<br>";
      }
-       
+    ?>
+    <h2>snack 5</h2>
+    <?php 
+        foreach($newParagrafo as $new){
+            echo "<p>$new</p>";
+        }    
     ?>
   
 </body>
