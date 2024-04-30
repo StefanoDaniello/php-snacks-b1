be46e0d0a4b28d22b519ce2f856544cf9cd76068
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-30 16:06:41 +0200

    snack 3

--- index.php
+++ index.php
@@ -32,6 +32,46 @@
             $acesso= "Accesso negato";
         }
     }
+    // snack3
+    $posts = [
+
+        '10/01/2019' => [
+            [
+                'title' => 'Post 1',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 1'
+            ],
+            [
+                'title' => 'Post 2',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 2'
+            ],
+        ],
+        '10/02/2019' => [
+            [
+                'title' => 'Post 3',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 3'
+            ]
+        ],
+        '15/05/2019' => [
+            [
+                'title' => 'Post 4',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 4'
+            ],
+            [
+                'title' => 'Post 5',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 5'
+            ],
+            [
+                'title' => 'Post 6',
+                'author' => 'Michele Papagni',
+                'text' => 'Testo post 6'
+            ]
+        ],
+    ];
 
 ?>
 
@@ -57,8 +97,19 @@
         <button type="submit">Invia</button>
         <?php echo $acesso; ?>
     </form>
-
-
+    <h2>snack 3</h2>
+    <?php 
+        foreach($posts as $date => $postList){
+            echo "<h3>$date</h3>";
+            foreach($postList as $post){
+                // echo $post['title'];
+                echo "<h4>{$post['title']}</h4>"; 
+                echo "<p><strong>Autore:</strong> {$post['author']}</p>"; 
+                echo "<p>{$post['text']}</p>";
+                echo "<hr>"; //linea divisoria 
+            }
+        }
+    ?>
 
 </body>
 </html>
